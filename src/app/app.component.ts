82383e65e3622fb0f2d0f577575c924b56b68542
Robert Villarreal <robertv1979@gmail.com>
2017-08-08 17:06:26 -0700

    Initial and Final commit for retro bar code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title: String = "Retro Bar Code"
+    colorArray = ['Aqua', 'BlueViolet', 'Chartreuse', 'Gold', 'Indigo', 'Lime', 'OrangeRed'];
 }
